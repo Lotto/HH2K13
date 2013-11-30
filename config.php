5fe46c1bfbe4552b9e770ba3a22f7a45c3e701ed
eXorus <exorus.spam@gmail.com>
2013-11-30 12:45:03 +0100

    squelette

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+define("WEBSITE_ENV", "dev");
+define("WEBSITE_VERSION", "0.1");
+define("WEBSITE_NAME", "HH2K13");
+define("WEBSITE_SNAME", "Les découpeurs");
+define("WEBSITE_LONG_NAME", WEBSITE_NAME." | ".WEBSITE_SNAME);
+define("WEBSITE_SMALL_NAME", " | ".WEBSITE_SNAME);
+
+define("WEBSITE_LINK", "http://ogre-rouen.fr/HH2K13/");
+if ( !defined("WEBSITE_PATH") )
+	define("WEBSITE_PATH", dirname(__FILE__) . "/");
+
+define("DATABASE_HOST", "localhost");
+define("DATABASE_NAME", "HH2K13"); 
+define("DATABASE_LOGIN", "HH2K13");
+define("DATABASE_PASSWORD", "HH2K13");
+
+//Authorized Apps
+define("DEFAULT_APP", "home");
+$authorizedApps = array(
+	"home" 			=> "home",
+	"new"		=> "new",
+	"errors"		=> "errors");
+
+//Error Manage
+if(WEBSITE_ENV == "prod"){
+	ini_set("display_errors", 0);
+	ini_set("log_errors", 1);
+	error_reporting(E_ALL);
+}
+else{
+	error_reporting(E_ALL);
+}
+?>
--- index.php
+++ index.php
@@ -1,5 +1,48 @@
 <?php
-echo "###";
 echo "Hello World !";
-echo "###";
+
+
+//Session Load
+session_start();
+
+//Configuration Load
+require_once("config.php");
+
+//Autoload
+function __autoload($class_name) {
+    require_once(WEBSITE_PATH.'inc/class/'.$class_name.'.class.php');
+}
+
+
+//Dispatcher
+if(isset($_GET['app']) && !empty($_GET['app'])){
+	if(array_key_exists($_GET['app'], $authorizedApps)){
+		
+		$appToCall = $authorizedApps[$_GET['app']];
+		
+		$do = (isset($_GET['do']) && !empty($_GET['do'])) ? $_GET['do'] : $appToCall;
+	}
+	else{
+		header('HTTP/1.0 404 Not Found');
+		exit();
+	}
+}
+else{
+	$appToCall = $authorizedApps[DEFAULT_APP];
+	$do = DEFAULT_APP;
+}
+
+//Master Controller
+if(is_file('./apps/'.$appToCall.'/'.$appToCall.'.controller.class.php') ){
+	
+	require_once(WEBSITE_PATH.'apps/'.$appToCall.'/'.$appToCall.'.controller.class.php');
+		
+	$dispatcherToCall = $appToCall.'Controller';
+	$d = new $dispatcherToCall();
+	$d->$do();
+}
+else{
+	header('HTTP/1.0 404 Not Found');
+    exit();
+}
 ?>
