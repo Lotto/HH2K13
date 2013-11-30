2a461557ba7bd2c17a4042dfc964536273fbf6c7
Thibaud Dauce <thibaud.dauce@quantic-telecom.net>
2013-12-01 00:42:47 +0100

    Passage du corps de la fonction dans la condition sans fatal error

--- apps/crop/crop.controller.class.php
+++ apps/crop/crop.controller.class.php
@@ -12,52 +12,53 @@ class cropController{
 		$r->execute(array($idCrop));
 		$crops = $r->fetchAll();
 
-		if (!empty($crops))
+		if (!empty($crops)) {
 			$crop = $crops[0];
-		else
-			$erreurFatale = "Aïe :( Cet identifiant n'est associé à aucun crop. <a href=\"".WEBSITE_LINK.projects"\">Retournez à la page des projets ?</a>";
 
-		$message = "Propose nous ton Piczle de ".$crop->WIDTH."px de long par ".$crop->HEIGHT."px de hauteur !";
+			$message = "Propose nous ton Piczle de ".$crop->WIDTH."px de long par ".$crop->HEIGHT."px de hauteur !";
 
-		if(!empty($_FILES['piczle']))
-		{
-			require_once('phar://'.WEBSITE_PATH.DS."lib".DS."imagine".DS.'imagine.phar');
-			require_once(WEBSITE_PATH.DS.'inc'.DS.'class'.DS.'SPDO.class.php');
-			
-			$image = $_FILES['piczle'];
-
-			if (empty($image['tmp_name']))
-				$erreur = 'Oh... :( Impossible de récupérer votre Piczle';
-			else
+			if(!empty($_FILES['piczle']))
 			{
-				$imagine = new Imagine\Gd\Imagine();
+				require_once('phar://'.WEBSITE_PATH.DS."lib".DS."imagine".DS.'imagine.phar');
+				require_once(WEBSITE_PATH.DS.'inc'.DS.'class'.DS.'SPDO.class.php');
 				
-				$image = $imagine->open($image['tmp_name']);
-				$width = $image->getSize()->getWidth();
-				$height = $image->getSize()->getHeight();
-				
-				if ($width != $crop->WIDTH OR $height != $crop->HEIGHT)
-					$erreur = "Votre Piczle ne fait pas la bonne taille :( Assurez de vous de fournir un Piczle de ".$crop->WIDTH."px de long par ".$crop->HEIGHT."px de hauteur";
-				else {
+				$image = $_FILES['piczle'];
 
-					$insert = SPDO::getInstance()->prepare("INSERT INTO PHOTOS_CROP(ID_CROP) VALUES(:idCrop)");
-					$insert->execute(array(
-						'idCrop' => $idCrop
-						));
-					$id = SPDO::getInstance()->lastInsertId();
+				if (empty($image['tmp_name']))
+					$erreur = 'Oh... :( Impossible de récupérer votre Piczle';
+				else
+				{
+					$imagine = new Imagine\Gd\Imagine();
 					
-					try {
+					$image = $imagine->open($image['tmp_name']);
+					$width = $image->getSize()->getWidth();
+					$height = $image->getSize()->getHeight();
+					
+					if ($width != $crop->WIDTH OR $height != $crop->HEIGHT)
+						$erreur = "Votre Piczle ne fait pas la bonne taille :( Assurez de vous de fournir un Piczle de ".$crop->WIDTH."px de long par ".$crop->HEIGHT."px de hauteur";
+					else {
 
-						$image->save(WEBSITE_PATH.DS.'data'.DS.'piczle'.DS.$id.'.jpg');
-					} catch (Exception $e) {
-						$erreur = "Impossible d'enregistrer votre Piczle :( Veuillez réessayer plus tard.";
+						$insert = SPDO::getInstance()->prepare("INSERT INTO PHOTOS_CROP(ID_CROP) VALUES(:idCrop)");
+						$insert->execute(array(
+							'idCrop' => $idCrop
+							));
+						$id = SPDO::getInstance()->lastInsertId();
+						
+						try {
+
+							$image->save(WEBSITE_PATH.DS.'data'.DS.'piczle'.DS.$id.'.jpg');
+						} catch (Exception $e) {
+							$erreur = "Impossible d'enregistrer votre Piczle :( Veuillez réessayer plus tard.";
+						}
+						
+						header('Location: '.WEBSITE_LINK);
 					}
-					
-					header('Location: '.WEBSITE_LINK);
-				}
 
+				}
 			}
 		}
+		else
+			$erreurFatale = 'Aïe :( Cet identifiant n\'est associé à aucun crop. <a href="'.WEBSITE_LINK.'projects">Retournez à la page des projets ?</a>';
 
 		require_once("upload.view.php");
 	}
