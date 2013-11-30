4cd5cd75dd765a4a4fb5dbb26a4d1181d4cad630
Thibaud Dauce <thibaud.dauce@quantic-telecom.net>
2013-11-30 20:46:47 +0100

    Correction de syntaxe pour un if html

--- apps/master/master.view.php
+++ apps/master/master.view.php
@@ -1,10 +1,10 @@
 <?php require_once(WEBSITE_PATH."tpl/default_fo/header.tpl.php"); ?>
 
-<?php if (isset($message)) { ?>
-    <div class="alert alert-danger">
-        <?php echo $message ?>
-    </div>
-<?php } ?>
+<?php if (isset($message)): ?>
+	<div class="alert alert-danger">
+		<?php echo $message ?>
+	</div>
+<?php endif ?>
 
 <h1>Uploader ma photo</h1>
 <form action="" method="post" enctype="multipart/form-data">
