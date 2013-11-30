4448f0d514495dc163e7ddc6ed89bd9dab7aef1f
eXorus <exorus.spam@gmail.com>
2013-11-30 16:36:17 +0100

    master photo

--- apps/home/home.view.php
+++ apps/home/home.view.php
@@ -2,9 +2,9 @@
 require_once(WEBSITE_PATH."tpl/default_fo/header.tpl.php");
 
 foreach($photosMaster as $master){
-	echo $master->ID;
-	echo $master->WIDTH;
-	echo $master->HEIGHT;
+	//echo $master->ID;
+	//echo $master->WIDTH;
+	//echo $master->HEIGHT;
 	echo '<img src="'.WEBSITE_LINK.'data'.DS.'master'.DS.$master->ID.'.jpg" alt="Photo">';
 	echo "<br />";
 }
