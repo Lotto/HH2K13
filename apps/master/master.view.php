41001b6ef41d34854fd86ddf9dcdd732027756e2
Lotto <cauchois.k@gmail.com>
2013-11-30 20:09:38 +0100

    debut template

--- apps/master/master.view.php
+++ apps/master/master.view.php
@@ -1,5 +1,15 @@
+<?php require_once(WEBSITE_PATH."tpl/default_fo/header.tpl.php"); ?>
+
+<?php if (isset($message)) { ?>
+    <div class="alert alert-danger">
+        <?php echo $message ?>
+    </div>
+<?php } ?>
+
 <h1>Uploader ma photo</h1>
 <form action="" method="post" enctype="multipart/form-data">
 	<label for="master">Image</label><input type="file" id="master" name="master">
 	<input type="submit" value="Envoyer ma photo">
 </form>
+
+<?php require_once(WEBSITE_PATH."tpl/default_fo/footer.tpl.php"); ?>
