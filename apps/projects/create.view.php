d52db7c2347fc2b905d5699aa6ae94bc3108a03f
Thibaud Dauce <thibaud.dauce@quantic-telecom.net>
2013-12-01 00:39:51 +0100

    Correction d'un bug

--- apps/projects/create.view.php
+++ apps/projects/create.view.php
@@ -2,7 +2,7 @@
 require_once(WEBSITE_PATH."tpl/default_fo/header.tpl.php");
 ?>
 
-<button id="ajouter">Ajoutez un crop de Piczle</button>
+<button id="ajouter">Ajoutez des crops' Piczle</button>
 	<section id="mainPhoto" style="background-image: url('<?php echo WEBSITE_LINK; ?>data/master/<?php echo $photoMaster->ID; ?>.jpg'); width:<?php echo $photoMaster->WIDTH; ?>px; height:<?php echo $photoMaster->HEIGHT; ?>px"></section>
 
 	<form action="" method="post" id="form">
