63319e8bddbf0c880678012a1e92b50584f0dd4a
Thibaud Dauce <thibaud.dauce@quantic-telecom.net>
2013-11-30 17:10:39 +0100

    Création d'un url rewriting qui marche

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ session_start();
 
 //Configuration Load
 require_once("config.php");
+require_once("lib/functions.php");
 
 //Autoload
 function __autoload($class_name) {
@@ -12,12 +13,17 @@ function __autoload($class_name) {
 
 
 //Dispatcher
-if(isset($_GET['app']) && !empty($_GET['app'])){
-	if(array_key_exists($_GET['app'], $authorizedApps)){
+$get = explode('/', $_GET['arguments']);
+$getApp = (isset($get[0]) && !empty($get[0])) ? $get[0] : '';
+$getDo = (isset($get[1]) && !empty($get[1])) ? $get[1] : '';
+$get = array_slice($get, '2');
+
+if(isset($getApp) && !empty($getApp)){
+	if(array_key_exists($getApp, $authorizedApps)){
 		
-		$appToCall = $authorizedApps[$_GET['app']];
+		$appToCall = $authorizedApps[$getApp];
 		
-		$do = (isset($_GET['do']) && !empty($_GET['do'])) ? $_GET['do'] : $appToCall;
+		$do = (isset($getDo) && !empty($getDo)) ? $getDo : $appToCall;
 	}
 	else{
 		header('HTTP/1.0 404 Not Found');
@@ -35,7 +41,9 @@ if(is_file('./apps/'.$appToCall.'/'.$appToCall.'.controller.class.php') ){
 	require_once(WEBSITE_PATH.'apps/'.$appToCall.'/'.$appToCall.'.controller.class.php');
 		
 	$dispatcherToCall = $appToCall.'Controller';
+	debug($get);
 	$d = new $dispatcherToCall();
+	$d->params = $get;
 	$d->$do();
 }
 else{
