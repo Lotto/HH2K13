5d0573966e6c0a42d23effb2f792d6b48b0f174b
eXorus <exorus.spam@gmail.com>
2013-11-30 16:41:23 +0100

    fix issue

--- apps/home/home.controller.class.php
+++ apps/home/home.controller.class.php
@@ -4,19 +4,14 @@ class homeController{
 	
 	function home(){
 
-
 		$r = SPDO::getInstance()->prepare("SELECT * FROM  PHOTOS_MASTER");
 		$r->setFetchMode(PDO::FETCH_OBJ);
 		$r->execute();
 		$photosMaster = $r->fetchAll();
-		print_r($photosMaster);
-		
-
 
 		//require_once('phar://'.WEBSITE_PATH.DS."lib".DS."imagine".DS.'imagine.phar');
 		//$imagine = new Imagine\Gd\Imagine();
 		//$image = $imagine->open(WEBSITE_PATH.DS.'data'.DS.'master'.DS.'1.jpg');
-
 
 		require_once("home.view.php");
 	}
