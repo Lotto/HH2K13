10c41fd2fb7c655d8437dabe9547989187cc2ee8
Lotto <cauchois.k@gmail.com>
2013-11-30 20:19:15 +0100

    home

--- apps/home/home.view.php
+++ apps/home/home.view.php
@@ -1,13 +1,25 @@
+<?php require_once(WEBSITE_PATH."tpl/default_fo/header.tpl.php"); ?>
+
+<div class="jumbotron">
+    <h1>Piczle Me</h1>
+    <p class="lead"></p>
+    <p>
+        <a class="btn btn-success" href="<?php echo WEBSITE_LINK ?>master" role="button">Créer un projet</a>
+        <a class="btn btn-success" href="#" role="button">Participer à un projet</a>
+    </p>
+</div>
+
+<div class="row">
 <?php
-require_once(WEBSITE_PATH."tpl/default_fo/header.tpl.php");
-
-foreach($photosMaster as $master){
-	//echo $master->ID;
-	//echo $master->WIDTH;
-	//echo $master->HEIGHT;
-	echo '<img src="'.WEBSITE_LINK.'data'.DS.'master'.DS.$master->ID.'.jpg" alt="Photo">';
-	echo "<br />";
+foreach($photosMaster as $master) {
+    echo '<div class="col-lg-6">';
+    echo '<img src="'.WEBSITE_LINK.'data'.DS.'master'.DS.$master->ID.'.jpg" alt="Photo">';
+    echo '</div>';
+    echo '<div class="col-lg-6">';
+    echo '<img src="'.WEBSITE_LINK.'data'.DS.'master'.DS.$master->ID.'.jpg" alt="Photo">';
+    echo '</div>';
 }
-
-require_once(WEBSITE_PATH."tpl/default_fo/footer.tpl.php");
 ?>
+</div>
+
+<?php require_once(WEBSITE_PATH."tpl/default_fo/footer.tpl.php");?>
