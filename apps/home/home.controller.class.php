1d918151cade7353268dc6b565dd36dee6bbc606
root <root@crunchbang.(none)>
2013-12-01 03:58:48 +0100

    debut du download

--- apps/home/home.controller.class.php
+++ apps/home/home.controller.class.php
@@ -17,12 +17,5 @@ class homeController{
 		require_once("home.view.php");
 	}
 
-    function download() {
-        $type = $this->params[0];
-        $id = $this->params[1];
-        $path = WEBSITE_LINK . "data/" . $type . "/" . $id . ".jpg";
-        require_once("home.download.php");
-    }
-
 }
 ?>
